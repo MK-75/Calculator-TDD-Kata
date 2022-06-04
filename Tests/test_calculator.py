2aecdcc8f0e1766369b6ef429419cb9911629cec
MK-75 <mkparekh2803@gmail.com>
2022-06-04 09:29:08 +0530

    Addition for empty string

--- Tests/test_calculator.py
+++ Tests/test_calculator.py
@@ -1,1 +1,9 @@
 # Test cases for the calculator code
+
+import pytest
+from calculator import add
+
+# Test for addition of two numbers
+def test_add():
+    result = add(" ")
+    assert result == 0
--- calculator.py
+++ calculator.py
@@ -1,1 +1,15 @@
 # String calculator code
+
+def add(number):
+    """Addition of two numbers
+
+    Args:
+        number (str): string containing numbers to add
+    
+    Returns:
+        int: addition of two numbers
+    """
+    if number == " ":
+        return 0
+    else:
+        return 1
